9c39abb130c51f6f1416f46c8c7453f17515f9a4
Abdulaziz Abduvakhobov <abdulaziz.yosk@gmail.com>
2023-02-15 15:57:52 +0300

    changed User model

--- api/models.py
+++ api/models.py
@@ -1,14 +1,32 @@
-from django.contrib.auth.hashers import make_password, check_password
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 
-# Create your models here.
 class Category(models.Model):
     name = models.CharField(max_length=100)
     
-    def __str__(self) -> str:
+    def __str__(self):
         return self.name
-        
-class User(models.Model):
+    
+    class Meta:
+        verbose_name = "Category"
+        verbose_name_plural = "Categories"
+
+class CustomUserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError('The Email field must be set')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save()
+        return user
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        return self.create_user(email, password, **extra_fields)
+
+class User(AbstractBaseUser):
     MALE = 'M'
     FEMALE = 'F'
     GENDER_CHOICES = (
@@ -19,21 +37,27 @@ class User(models.Model):
     email = models.EmailField(unique=True)
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
-    identity_number = models.IntegerField(unique=True)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+    identity_number = models.IntegerField(null=True, blank=True)
     gender = models.CharField(max_length=1, choices=GENDER_CHOICES)
-    birthdate = models.DateField(auto_now_add=True)
+    birth_date = models.DateField(auto_now_add=True)
     weight = models.IntegerField(blank=True, null=True)
     height = models.IntegerField(blank=True, null=True)
     address = models.TextField()
-    category = models.ForeignKey(Category, on_delete=models.CASCADE)
-    password = models.CharField(max_length=256)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, default=1)
     
-    def __str__(self):
-        return f"{self.first_name} {self.last_name} | {self.email}"
-    
-    def set_password(self, raw_password):
-        self.password = make_password(raw_password)
-        self.save()
+    objects = CustomUserManager()
 
-    def check_password(self, raw_password):
-        return check_password(raw_password, self.password)
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['first_name', 'last_name', 'gender', 'birth_date', 'address']
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name} | {self.email}'
+
+    def has_perm(self, perm, obj=None):
+        return True
+
+    def has_module_perms(self, app_label):
+        return True
