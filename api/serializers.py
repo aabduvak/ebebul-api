1c450de6dfae46f8f6660189af28e61a3ba59acf
Abdulaziz Abduvakhobov <abdulaziz.yosk@gmail.com>
2023-02-17 05:28:05 +0300

    added default group to user model

--- api/serializers.py
+++ api/serializers.py
@@ -1,15 +1,23 @@
 from rest_framework import serializers
 from rest_framework.permissions import IsAuthenticated
+from django.contrib.auth.models import Group
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
-from .models import User, Group
+from .models import User
+
+@receiver(post_save, sender=User)
+def set_default_group(sender, instance, created, **kwargs):
+    if created:
+        instance.groups.add(Group.objects.get(name='User'))
 
 class UserSerializer(serializers.ModelSerializer):
     permission_classes = [IsAuthenticated]
     class Meta:
         model = User
-        exclude = ('is_staff', 'is_superuser', 'is_active', 'groups', 'user_permissions')
+        exclude = ('is_staff', 'is_superuser', 'is_active', 'user_permissions')
         extra_kwargs = {'password': {'write_only': True}}
-    
+      
     def update(self, instance, validated_data):
         password = validated_data.pop('password', None)
 
