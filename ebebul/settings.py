8df91d2bb112766f266d41ab5459c4550512e0b0
Abdulaziz Abduvakhobov <abdulaziz.yosk@gmail.com>
2023-04-15 03:31:28 +0500

    Changed sqlite to postgresql

--- ebebul/settings.py
+++ ebebul/settings.py
@@ -34,6 +34,13 @@ DEBUG = env('DEBUG', default=True)
 
 ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', default=[])
 
+# DATABASE SETTINGS
+DB_NAME = env('DB_NAME')
+DB_USER = env('DB_USER')
+DB_PASSWORD = env('DB_PASSWORD')
+DB_HOST = env('DB_HOST')
+DB_PORT = env('DB_PORT')
+
 # Application definition
 
 INSTALLED_APPS = [
@@ -85,8 +92,12 @@ WSGI_APPLICATION = 'ebebul.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': DB_NAME,
+        'USER': DB_USER,
+        'PASSWORD': DB_PASSWORD,
+        'HOST': DB_HOST,
+        'PORT': DB_PORT
     }
 }
 
